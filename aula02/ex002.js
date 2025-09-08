f1ac6af76288856e631a37eff2598049d5160018
caroltxdev <carolinateixeira.150@gmail.com>
2025-09-08 17:47:52 -0300

    uptade

--- aula02/ex002.js
+++ aula02/ex002.js
@@ -9,13 +9,34 @@ Instruções:
 as temperaturas do dia.
 
 2. Crie três arrays vazios:
-o frios
-o agradaveis
-o quentes
+- frios
+- agradaveis
+- quentes
 3. Use um laço for para percorrer cada item do array temperaturas.
 
 4. Use if / else if / else para verificar a temperatura e adicionar no array
 correspondente.
 
 5. No final, exiba todos os arrays no console.*/
+
+let temperatura = [10, 15, 18, 19, 20, 21, 22, 25, 27, 28, 30, 32];
+let frio = [];
+let agradavel = [];
+let quente = [];
+
+    for(let i = 0; i < temperatura.length; i++ ) {
+
+        let temp = temperatura[i];
+
+        if(i < 20){
+            frio.push(temp);
+            console.log('O clima está frio!', frio);
+        }else if(i >= 20 && i <= 29){
+            agradavel.push(temp);
+            console.log('O clima está agradável!', agradavel);
+        }else{
+            quente.push(temp);
+            console.log('O clima está quente', quente);
+        }
+    }
 
